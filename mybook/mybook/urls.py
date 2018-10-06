22d85930afcb570b246f06ce21d414f3576f9e1d
admin <admin@example.com>
2018-10-06 14:53:19 +0900

    aaaaa

--- mybook/mybook/urls.py
+++ mybook/mybook/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include   # ←, includeを追加
 
 urlpatterns = [
     path('cms/', include('cms.urls')),   # ←ここを追加
+    path('api/', include('api.urls')),  # ここを追加
     path('admin/', admin.site.urls),
 ]
